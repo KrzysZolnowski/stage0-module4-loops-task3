92352e20b692b1718f9ee58f3f10d1b7f80c00e5
Krzysztof <krzys.zolnowski@gmail.com>
2024-08-16 00:08:17 +0200

    4 task of fourth module done

--- src/main/java/school/mjc/stage0/loops/task3/NinesSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/NinesSum.java
@@ -1,7 +1,16 @@
 package school.mjc.stage0.loops.task3;
 
 public class NinesSum {
-    public void calculateSum(int lengthOfLastNumber) {
+    public long calculateSum(int lengthOfLastNumber) {
+        long sum = 0;
+        long term = 0;
+
+        for (int i = 1; i <= lengthOfLastNumber; i++) {
+            term = term * 10 + 9; // Generate the ith term
+            sum += term; // Add the term to the sum
+        }
+
+        return sum;
+    }
 
     }
-}
